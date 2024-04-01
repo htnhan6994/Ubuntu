7d30c308f2967c398f7cb43baeef00997c7df376
htnhan6994 <139978509+htnhan6994@users.noreply.github.com>
2024-04-01 23:55:57 +0700

    Update snort3_install.sh

--- snort3_install.sh
+++ snort3_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd snort3
-# Chạy lệnh ansible-playbook
-ansible-playbook install_snort3.yml -K -i inventory
+# Run command ansible-playbook to install snort3
+ansible-playbook install.yml -K -i inventory

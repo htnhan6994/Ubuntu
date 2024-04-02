8f7e266bfffa422be8f26a71a173dc20f8a66b91
htnhan6994 <139978509+htnhan6994@users.noreply.github.com>
2024-04-02 09:37:26 +0700

    Update snort3_install.sh

--- snort3_install.sh
+++ snort3_install.sh
@@ -3,3 +3,5 @@
 cd snort3
 # Run command ansible-playbook to install snort3
 ansible-playbook install.yml -K -i inventory
+ansible-playbook config_network_interfaces.yml -K -i inventory
+ansible-playbook pulledpork3_install.yml -K -i inventory
